a7a58c974df429c2d1899df778cbb8328e3d676f
suhail <suhailsqm@gmail.com>
2021-05-29 20:28:05 +0530

    removing duplicate header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './common/header/header';
 import './index.css';
 import Home from "./screens/home/home";
 
@@ -8,7 +7,6 @@ import Home from "./screens/home/home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header></Header>
     <Home />
   </React.StrictMode>,
   document.getElementById('root')
